a7a0131b4bd3939c3817b03b8b23b0d3f7cc8a5a
System233 <cdkeylzh@outlook.com>
2020-12-15 23:19:37 +0800

    添加消息测试Action

--- send.ts
+++ send.ts
@@ -2,7 +2,6 @@ import { SendMessage } from "./src"
 
 (async()=>{
     try {
-        console.log(process.argv)
         const resp=await SendMessage(process.argv[2],process.argv.slice(3).join());
         console.log(resp.data)
     } catch (error) {
