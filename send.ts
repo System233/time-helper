ced6d5af4ddd759dbf2be5846b86e3d2e31107b3
System233 <cdkeylzh@outlook.com>
2020-12-16 00:41:43 +0800

    更新Action

--- send.ts
+++ send.ts
@@ -3,7 +3,10 @@ import { SendMessage } from "./src"
 (async()=>{
     try {
         const data=await SendMessage(process.argv[2],process.argv.slice(3).join());
-        console.log(data);
+        if(data&&data.errno){
+            console.error(`Error(${data.errno})：`,data.errmsg);
+            process.exit(data.errno);
+        }
     } catch (error) {
         console.error(error);
         process.exit(-1);
